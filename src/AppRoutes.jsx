48c76e63b05a026490fe7538934c1bc652f4d7cc
icanq <icanq@hotmail.com>
2022-03-22 20:06:32 +0700

    feat: setup routes

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Todos from './pages/Todos';
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/todo' element={<Todos />} />
+      {/* 404 pages */}
+      <Route path='/404' element={<NotFound />} />
+      {/* Handler to redirect to 404 pages */}
+      <Route path='/*' element={<Navigate to='/404' />} />
+    </Routes>
+  );
+}
