74f4fc96b6c62cbbe24ae414262b08c49351c72c
icanq <icanq@hotmail.com>
2022-03-22 21:20:51 +0700

    feat: change complete status

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -5,10 +5,19 @@ import dataTodo from '../data/todo.json';
 export default function Todos() {
   const [todo, setTodo] = useState(dataTodo);
 
+  const handleComplete = (id) => {
+    let mappedTodo = todo.map((t) => {
+      return t.id === Number(id) ? { ...t, complete: !t.complete } : { ...t };
+    });
+    setTodo(mappedTodo);
+  };
+
   const addTodo = (userInput) => {
     let copy = [...todo];
     copy = [...copy, { id: todo.length + 1, task: userInput, complete: false }];
     setTodo(copy);
+    // perubahan data pasti di handle dari bagian backend, maka dari itu ini kita hanya coba untuk mock datanya aja
+    // fs.writeFileSync('../data/todo.json', copy, 'utf-8');
   };
 
   return (
@@ -19,7 +28,7 @@ export default function Todos() {
         {todo.map((e, index) => {
           return (
             <li
-              onClick={() => console.log(e.id)}
+              onClick={() => handleComplete(e.id)}
               className={e.complete ? 'strike' : ''}
               key={index}
             >
